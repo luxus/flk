9a9d61fedcfab659a3bbd896a5d21b42b3ff3163
luxus <luxuspur@gmail.com>
2022-11-30 07:25:06 +0100

    adding my keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,38 +1,37 @@
 let
   # set ssh public keys here for your system and user
-  pik2 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPX+ah50jmnLkmDPjSc7fN6+bLVdfPtABgncHgx9qwG5";
-  themachine = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC+SPYr7r+FSy/IY/TI4VYOKFkZ2X5yjsj8eYSIuvDRO";
-  systems = [themachine pik2];
+  cecile = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIARseoTkm2cogxfJA3tN5l+obzOZxqqY+ufz1yet1jgi";
+  emily = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIANPACt5QZONEUPsjjt3J/zFbC3UHxzPhHTFiegF9k1A";
+  systems = [emily cecile];
 
-  alita = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIENcgkaR9Eut882LrniZvXzZ26ik5JWZzyNG7odZ172d";
-  danie = "age1yubikey1q293cy2t04vz0aallsfcvy5qgk3tmw7rnmz9e6syhcf3esawxfdcu6e9ghn";
-  defaultUsers = [danie];
-  users = defaultUsers ++ [alita];
+  luxus = "age1ncepfcrzfep624tpanhqfflafznh7ftuqffv0nfq24jhv3dwx40sltjkad";
+  defaultUsers = [luxus];
+  users = defaultUsers ;
 
   allKeys = systems ++ users;
 in {
   "home/users/root.age".publicKeys = allKeys;
-  "home/users/alita.age".publicKeys = [alita pik2] ++ [danie themachine];
-  "home/users/danie.age".publicKeys = [danie themachine];
+  "home/users/seed.age".publicKeys = [cecile] ++ [luxus emily];
+  "home/users/luxus.age".publicKeys = [luxus emily];
 
-  "home/profiles/accounts.age".publicKeys = defaultUsers ++ [themachine];
+  "home/profiles/accounts.age".publicKeys = defaultUsers ++ [emily];
 
-  "nixos/profiles/cloud/caddy.age".publicKeys = [alita pik2] ++ [danie themachine];
+  "nixos/profiles/cloud/caddy.age".publicKeys = [cecile] ++ [luxus emily];
 
-  "nixos/profiles/cloud/calibre-server/users.sqlite.age".publicKeys = [alita pik2] ++ [danie themachine];
+  "nixos/profiles/cloud/calibre-server/users.sqlite.age".publicKeys = [cecile] ++ [luxus emily];
 
-  "nixos/profiles/cloud/cloudflare.age".publicKeys = [alita pik2] ++ [danie themachine];
+  "nixos/profiles/cloud/cloudflare.age".publicKeys = [cecile] ++ [luxus emily];
 
-  "nixos/profiles/cloud/etebase-server/superuser.env.age".publicKeys = [alita pik2] ++ [danie themachine];
+  "nixos/profiles/cloud/etebase-server/superuser.env.age".publicKeys = [cecile] ++ [luxus emily];
 
-  "nixos/profiles/cloud/etebase-server/secret.txt.age".publicKeys = [alita pik2] ++ [danie themachine];
+  "nixos/profiles/cloud/etebase-server/secret.txt.age".publicKeys = [cecile] ++ [luxus emily];
 
-  "nixos/profiles/cloud/duckdns.age".publicKeys = [alita pik2] ++ [danie themachine];
+  "nixos/profiles/cloud/duckdns.age".publicKeys = [cecile] ++ [luxus emily];
 
-  "nixos/profiles/cloud/minecraft/ops.age".publicKeys = [alita pik2] ++ [danie themachine];
-  "nixos/profiles/cloud/minecraft/whitelist.age".publicKeys = [alita pik2] ++ [danie themachine];
+  "nixos/profiles/cloud/minecraft/ops.age".publicKeys = [cecile] ++ [luxus emily];
+  "nixos/profiles/cloud/minecraft/whitelist.age".publicKeys = [cecile] ++ [luxus emily];
 
-  "nixos/profiles/cloud/spotify.age".publicKeys = [alita pik2] ++ [danie themachine];
+  "nixos/profiles/cloud/spotify.age".publicKeys = [cecile] ++ [luxus emily];
 
-  "nixos/profiles/cloud/vaultwarden.age".publicKeys = [alita pik2] ++ [danie themachine];
+  "nixos/profiles/cloud/vaultwarden.age".publicKeys = [cecile] ++ [luxus emily];
 }
